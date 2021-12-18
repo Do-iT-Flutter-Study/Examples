bb22ddfb20b56d3ed4ae6d38dd6efa4ed9d28a30
Long9725 <develop9725@gmail.com>
2021-12-18 23:43:23 +0900

    Add bottomBar in homeScreen

--- lib/Screen/homeScreen.dart
+++ lib/Screen/homeScreen.dart
@@ -10,21 +10,60 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool _isObscure = true;
-  bool? _isChecked = false;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: <Widget>[Text("HomeScreen")],
-        ),
-      ),
-    );
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.amber,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.black38,
+            selectedFontSize: 14,
+            unselectedFontSize: 14,
+            currentIndex: _selectedIndex,
+            onTap: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home), title: Text('Home')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.star), title: Text('Favorite')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.menu), title: Text('Menu')),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.people), title: Text('My')),
+            ],
+          ),
+          body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+        ));
   }
+
+  List _widgetOptions = [
+    Text(
+      'HomeScreen',
+      style: TextStyle(fontSize: 40),
+    ),
+    Text(
+      'FavoriteScreen',
+      style: TextStyle(fontSize: 40),
+    ),
+    Text(
+      'MenuScreen',
+      style: TextStyle(fontSize: 40),
+    ),
+    Text(
+      'MyScreen',
+      style: TextStyle(fontSize: 40),
+    )
+  ];
 }
