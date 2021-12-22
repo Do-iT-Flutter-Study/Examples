5b2e8ec59ea81ad4535cf8d685809473c63f128a
Long9725 <develop9725@gmail.com>
2021-12-22 22:39:10 +0900

    facebook login

--- lib/src/page/login.dart
+++ lib/src/page/login.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
-import './home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:google_sign_in/google_sign_in.dart';
+
 import 'signUp.dart';
 import 'emailLogin.dart';
 
@@ -38,6 +42,29 @@ class _LoginState extends State<Login> {
   bool _isObscure = true;
   bool? _isChecked = false;
 
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final OAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final AccessToken result = (await FacebookAuth.instance.login()) as AccessToken;
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+    FacebookAuthProvider.credential(result.token);
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance
+        .signInWithCredential(facebookAuthCredential);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,11 +88,15 @@ class _LoginState extends State<Login> {
                 children: <Widget>[
                   SignInButton(
                     Buttons.Google,
-                    onPressed: () {},
+                    onPressed: () {
+                      signInWithGoogle();
+                    },
                   ),
                   SignInButton(
                     Buttons.Facebook,
-                    onPressed: () {},
+                    onPressed: () {
+                      signInWithFacebook();
+                    },
                   ),
                   SignInButton(
                     Buttons.Email,
